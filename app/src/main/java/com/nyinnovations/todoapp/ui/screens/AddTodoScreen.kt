22c77ff0a94dc3804b04f4ea2a621463e0c2195b
Neeraj Yadav <imnirajyadav@gmail.com>
2024-08-15 18:28:03 +0530

    add topbar and beautify UI a bit

--- app/src/main/java/com/nyinnovations/todoapp/ui/screens/AddTodoScreen.kt
+++ app/src/main/java/com/nyinnovations/todoapp/ui/screens/AddTodoScreen.kt
@@ -9,10 +9,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -22,6 +30,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -30,28 +39,44 @@ import com.nyinnovations.todoapp.data.TODOItem
 import com.nyinnovations.todoapp.ui.viewmodel.TODOListViewModel
 import kotlinx.coroutines.delay
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddTodoScreen(navController: NavController, todoViewModel: TODOListViewModel) {
     var text by rememberSaveable { mutableStateOf("") }
     var isLoading by remember { mutableStateOf(false) }
     var shouldLaunchEffect by remember { mutableStateOf(false) }
-    val statusBarHeight = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
 
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(top = statusBarHeight) // Padding for the status bar
-            .padding(16.dp),
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        TopAppBar(
+            title = { Text(text = stringResource(R.string.add_todo_title)) },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                titleContentColor = Color.White
+            ),
+            navigationIcon = {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = stringResource(R.string.back),
+                        tint = Color.White
+                    )
+                }
+            }
+        )
+
         OutlinedTextField(
             value = text,
             onValueChange = { text = it },
             label = { Text(stringResource(R.string.enter_todo_item)) },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth().padding(16.dp)
         )
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(18.dp))
 
         Button(onClick = {
             if (text.isNotBlank()) {
--- app/src/main/java/com/nyinnovations/todoapp/ui/screens/MainScreen.kt
+++ app/src/main/java/com/nyinnovations/todoapp/ui/screens/MainScreen.kt
@@ -1,15 +1,11 @@
 package com.nyinnovations.todoapp.ui.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -18,6 +14,8 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -26,6 +24,8 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -48,9 +48,6 @@ fun MainScreen(navController: NavController, todoViewModel: TODOListViewModel) {
     val todoItems by todoViewModel.filteredTodoItems.collectAsState()
     val query by todoViewModel.searchQuery.collectAsState()
 
-    // Get the status bar height
-    val statusBarHeight = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
-
     val errorMessage by todoViewModel.errorMessage.collectAsState(initial = "")
 
     // State to control the visibility of the AlertDialog
@@ -66,32 +63,40 @@ fun MainScreen(navController: NavController, todoViewModel: TODOListViewModel) {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = statusBarHeight) // Padding for the status bar
-                    .background(MaterialTheme.colorScheme.primary)
-                    .padding(8.dp)
-            ) {
+            Column {
+                TopAppBar(
+                    title = { Text(text = stringResource(R.string.app_name)) },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        titleContentColor = Color.White
+                    )
+                )
+
+                // Search bar below the toolbar
                 OutlinedTextField(
                     value = query,
                     onValueChange = { todoViewModel.onSearchQueryChanged(it) },
-                    label = { Text(stringResource(R.string.search_todos), color = Color.White.copy(alpha = 0.5f)) },
+                    label = {
+                        Text(
+                            stringResource(R.string.search_todos),
+                            color = Color.Black.copy(alpha = 0.5f)
+                        )
+                    },
                     modifier = Modifier
                         .fillMaxWidth()
-                        .background(Color.Transparent),
+                        .padding(8.dp),
                     colors = TextFieldDefaults.outlinedTextFieldColors(
-                        focusedTextColor = Color.White,
-                        cursorColor = Color.White,
-                        focusedLabelColor = Color.White.copy(alpha = 0.5f),
-                        focusedBorderColor = Color.White, // Transparent background
-                        unfocusedBorderColor = Color.Gray, // Transparent background
+                        focusedTextColor = Color.Black,
+                        cursorColor = Color.Black,
+                        focusedLabelColor = Color.Black.copy(alpha = 0.5f),
+                        focusedBorderColor = Color.Black,
+                        unfocusedBorderColor = Color.Gray,
                     ),
                     trailingIcon = {
                         Icon(
                             imageVector = Icons.Filled.Search,
                             contentDescription = stringResource(R.string.search),
-                            tint = Color.White
+                            tint = Color.Black
                         )
                     }
                 )
@@ -121,23 +126,24 @@ fun MainScreen(navController: NavController, todoViewModel: TODOListViewModel) {
                         contentPadding = PaddingValues(8.dp)
                     ) {
                         items(todoItems) { todoItem ->
-                            Text(
-                                text = todoItem.title,
+                            Card(
+                                shape = RoundedCornerShape(12.dp),
+                                elevation = CardDefaults.cardElevation(4.dp),
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.surface,
+                                    contentColor = MaterialTheme.colorScheme.onSurface
+                                ),
                                 modifier = Modifier
-                                    .padding(vertical = 4.dp)
-                                    .background(
-                                        MaterialTheme.colorScheme.surfaceDim,
-                                        RoundedCornerShape(2.dp)
-                                    )
-                                    .border(
-                                        0.2.dp,
-                                        MaterialTheme.colorScheme.outline,
-                                        RoundedCornerShape(2.dp)
-                                    )
                                     .fillMaxWidth()
-                                    .padding(12.dp),
-                                style = MaterialTheme.typography.bodyLarge
-                            )
+                                    .padding(vertical = 8.dp)
+                            ) {
+                                Text(
+                                    text = todoItem.title,
+                                    modifier = Modifier
+                                        .padding(16.dp),
+                                    style = MaterialTheme.typography.bodyLarge
+                                )
+                            }
                         }
                     }
                 }
